7b4ffa4cd7dceb7ffb9cf353471cf413bf810a3e
soldiersnake <mariano.maciasgandulfo@gmail.com>
2023-03-11 17:42:57 -0300

    agregado ruta de noticias y renderizado de imagen con BD

--- models/noticiasModels.js
+++ models/noticiasModels.js
@@ -1,13 +1,26 @@
-const pool = require('./../bd');
+const pool = require("./../bd");
 
-async function getNoticias(){
-    try{
-        let query = "select * from noticia";
-        let rows = await pool.query(query);
-        return rows;
-    }catch(error){
-        throw error;
-    }
+async function getNoticias() {
+  try {
+    let query =
+      "select *, DATE_FORMAT(fecha_creacion, '%d/%m/%Y') as fecha from noticia where estado = 1 order by id asc";
+    let rows = await pool.query(query);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
 }
 
-module.exports = {getNoticias};
+async function getNoticiasById(id) {
+  try {
+    let query =
+      "select *, DATE_FORMAT(fecha_creacion, '%d/%m/%Y') as fecha from noticia where id = ?";
+    // el ? se reemplaza con informacion a la hora de ejecutar la query
+    let rows = await pool.query(query, [id]);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { getNoticias, getNoticiasById };
--- routes/noticias.js
+++ routes/noticias.js
@@ -1,16 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const noticiasModel = require('./../models/noticiasModels');
+const noticiasModel = require("./../models/noticiasModels");
 
-
-router.get('/all', async (req, res, next)=>{
-    let noticias_array = await noticiasModel.getNoticias()
-    res.render('noticias', {noticias: noticias_array});
+router.get("/all", async (req, res, next) => {
+  let noticias_array = await noticiasModel.getNoticias();
+  res.render("noticias", { noticias: noticias_array });
 });
 
-router.get('/:id', (req, res, next)=>{
+router.get("/:id", async (req, res, next) => {
+  try {
     console.log(`El id que llega es: ${req.params.id}`);
-    res.end();
-})
+    let noticia = await noticiasModel.getNoticiasById(req.params.id);
+    res.render("noticia", { array_noticia: noticia });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
